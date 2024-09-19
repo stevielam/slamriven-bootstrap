0093c363c242397097473a03cc0ca8703a7686aa
Steve Lam <slam121212@gmail.com>
2024-09-19 14:10:54 -0700

    refactor

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 debridToken=${1}
+plexToken=${2}
 
-cp -rf /tmp/build/app/* /app/data
-chown -R 1000:1000 /app/data
+cp -rf /tmp/build/app/* /app
+chown -R 1000:1000 /app
 
 #connect Zurg to Real Debrid Account
-sed -i "s/replace_with_real_debrid_token/$debrid_token/g" /app/data/config/zurg/config.yml
+sed -i "s/real_debrid_token_here/$debridToken/g" /app/zurg/config/config.yml
+
+sed -i "s/plex_token_here/$plexToken/g" /app/zurg/config/plexupdate.sh
+
